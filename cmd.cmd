6a27918c75858837bb78e4dcd2903e41ac369b69
damine mohamed saad <m.sdaminesaad@gmail.com>
2021-01-19 19:20:39 +0100

    first commit

---
+++ cmd.cmd
@@ -0,0 +1,7 @@
+echo "# Discord-Bot" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/DevLancer-Dz/Discord-Bot.git
+git push -u origin main
