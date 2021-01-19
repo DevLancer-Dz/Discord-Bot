1f87ad1961630d11b402d37bf12f37c72cf27c43
damine mohamed saad <m.sdaminesaad@gmail.com>
2021-01-19 19:23:51 +0100

    remove file

--- cmd.cmd
+++
@@ -1,7 +0,0 @@
-echo "# Discord-Bot" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/DevLancer-Dz/Discord-Bot.git
-git push -u origin main
